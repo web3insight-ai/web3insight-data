071c641f1c485edbd8f97acc9c06128697c79b7b
justinzjj <zoujiajie2001@163.com>
2025-09-24 11:34:15 +0800

    添加新的 ignore路径 ，进一步减少 仓库中 数据文件

--- SQL/getDataBase.sql
+++ SQL/getDataBase.sql
@@ -1,21 +1,38 @@
-SELECT current_database();
+SELECT
+  current_database();
 
-SELECT column_name, data_type
-FROM information_schema.columns
-WHERE table_schema = 'data'
+SELECT
+  column_name,
+  data_type
+FROM
+  information_schema.columns
+WHERE
+  table_schema = 'data'
   AND table_name = 'actors';
 
-SELECT column_name, data_type
-FROM information_schema.columns
-WHERE table_schema = 'data'
+SELECT
+  column_name,
+  data_type
+FROM
+  information_schema.columns
+WHERE
+  table_schema = 'data'
   AND table_name = 'ecosystems';
 
-SELECT column_name, data_type
-FROM information_schema.columns
-WHERE table_schema = 'data'
+SELECT
+  column_name,
+  data_type
+FROM
+  information_schema.columns
+WHERE
+  table_schema = 'data'
   AND table_name = 'events';
 
-SELECT column_name, data_type
-FROM information_schema.columns
-WHERE table_schema = 'data'
+SELECT
+  column_name,
+  data_type
+FROM
+  information_schema.columns
+WHERE
+  table_schema = 'data'
   AND table_name = 'repos';
